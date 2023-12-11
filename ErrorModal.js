951f84868866ae9b89e276c2a3f3b77f5e1477e6
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-12-11 11:13:13 +0530

    Add files via upload

--- ErrorModal.js
+++ ErrorModal.js
@@ -1,21 +1,30 @@
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css'
-const ErrorModal = (props)=>{
-    return <>
-    <div className={classes.backdrop} onClick={props.handleerror}/>
-     <Card className={classes.modal}>
-        <header className={classes.header}>
-            <h2>{props.tite}</h2>
-        </header>
-        <div className={classes.content}>
-            <p>{props.message}</p>
-        </div>
+import  ReactDOM from 'react-dom';  
 
-        <footer className={classes.actions}>
-            <Button onClick={props.handleerror}>Okay</Button>
-        </footer>
-    </Card>
+const Backdrop = (props)=> {return <div className={classes.backdrop} onClick={props.handleerror}/>}
+
+const Modal = (props) =>{ return  <Card className={classes.modal}>
+<header className={classes.header}>
+    <h2>{props.title}</h2>
+</header>
+<div className={classes.content}>
+    <p>{props.message}</p>
+</div>
+
+<footer className={classes.actions}>
+    <Button onClick={props.handleerror}>Okay</Button>
+</footer>
+</Card>}
+
+const ErrorModal = (props)=>{
+
+   
+    return <>
+     { ReactDOM.createPortal(<Backdrop handleerror={props.handleerror}/>,document.getElementById('modals'))}
+     { ReactDOM.createPortal(<Modal handleerror={props.handleerror} title={props.title} message={props.message} />, document.getElementById('modalss')) }
+    
     </>
     
 }
